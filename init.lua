0903b79f43fa43ca58b5caa03d10310f97fc39ff
tevon <tevoneyabi@gmail.com>
2024-04-10 23:09:12 +0200

    ubuntu version

--- init.lua
+++ init.lua
@@ -37,3 +37,8 @@ require "nvchad.autocmds"
 vim.schedule(function()
   require "mappings"
 end)
+
+-- require'lspconfig'.rust_analyzer.setup{}
+
+vim.keymap.set('n', 'gl', '<cmd>lua vim.diagnostic.open_float()<cr>')
+vim.keymap.set('n', ']d', '<cmd>lua vim.diagnostic.goto_next()<cr>')
