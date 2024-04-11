211a3057dafd6ff331c4362ad16fa10ef72eb53b
roguecat0 <roguecat0@gmail.com>
2024-04-11 09:23:02 +0200

    statusline with col numbers, made gruvbox line number more readable

--- lua/chadrc.lua
+++ lua/chadrc.lua
@@ -5,14 +5,34 @@
 local M = {}
 
 M.ui = {
-	theme = "yoru",
+	theme = "gruvbox",
   nvdash = {
     load_on_startup = true,
-  }
+  },
 	-- hl_override = {
 	-- 	Comment = { italic = true },
 	-- 	["@comment"] = { italic = true },
 	-- },
+  statusline = {
+    theme = "minimal", -- default/vscode/vscode_colored/minimal
+    -- default/round/block/arrow separators work only for default statusline theme
+    -- round and block will work for minimal theme only
+    separator_style = "round",
+    order = nil,
+    modules = nil,
+  },
+  -- hl_override = {
+  --   MyHighlightGroup = {
+  --     grey = "white",
+  --   }
+  -- }
+  changed_themes = {
+    gruvbox = {
+      base_30 = {
+        grey = "#a3b8ef",
+      },
+    },
+  }
 }
 
 return M
