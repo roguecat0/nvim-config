3ca25546ea66b0e6b63ad693cc230d7c4bcc3222
roguecat0 <roguecat0@gmail.com>
2024-10-29 11:39:30 +0100

    added tmux compat plugin and mappings

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,5 +1,10 @@
 return {
   {
+    "christoomey/vim-tmux-navigator",
+    lazy=false
+
+  },
+  {
     "stevearc/conform.nvim",
     -- event = 'BufWritePre', -- uncomment for format on save
     opts = require "configs.conform",
