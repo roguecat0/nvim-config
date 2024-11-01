27b6e652b536f35104dc57508cd8dbc87192ba4b
roguecat0 <roguecat0@gmail.com>
2024-11-01 03:08:40 +0100

    added starter nice options

--- lua/options.lua
+++ lua/options.lua
@@ -2,5 +2,14 @@ require "nvchad.options"
 
 -- add yours here!
 
--- local o = vim.o
--- o.cursorlineopt ='both' -- to enable cursorline!
+local o = vim.o
+-- o.cursorlineopt = "both" -- to enable cursorline!
+
+o.scrolloff = 8
+o.number = true
+o.relativenumber = true
+
+o.hlsearch = true
+o.incsearch = true
+
+o.clipboard = "unnamed"
